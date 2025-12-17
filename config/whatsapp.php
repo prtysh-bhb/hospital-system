6a95f3fe3a7870cb8062c680a7bc6e401fef4fda
krunalk.brainerhub <krunalk.brainerhub@gmail.com>
2025-12-17 18:40:18 +0530

    WhatsApp Cloud API implements and test in Postman and test and debug

---
+++ config/whatsapp.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'api_url' => env('WHATSAPP_BUSINESS_API_URL', 'https://graph.facebook.com/v22.0'),
+    'phone_number_id' => env('WHATSAPP_BUSINESS_PHONE_NUMBER_ID'),
+    'business_account_id' => env('WHATSAPP_BUSINESS_ACCOUNT_ID'),
+    'access_token' => env('WHATSAPP_BUSINESS_ACCESS_TOKEN'),
+    'verify_token' => env('WHATSAPP_BUSINESS_VERIFY_TOKEN'),
+    'version' => 'v22.0',
+
+    'webhook' => [
+        'url' => env('APP_URL') . '/api/whatsapp/webhook',
+        'events' => [
+            'messages',
+            'message_template_status_update',
+            'message_template_quality_update',
+        ],
+    ],
+
+    'message_types' => [
+        'text' => 'text',
+        'image' => 'image',
+        'document' => 'document',
+        'audio' => 'audio',
+        'video' => 'video',
+        'sticker' => 'sticker',
+        'location' => 'location',
+        'contacts' => 'contacts',
+        'interactive' => 'interactive',
+        'template' => 'template',
+        'reaction' => 'reaction',
+    ],
+];
